a2f7321152a7de61f159f22b88e76e383dfef4b5
rajesh <rajesh.t7982@gmail.com>
2020-07-24 22:30:55 +0530

    updates to deploy again to  heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Ang10appmultiplemodules'));
+app.use(express.static(__dirname + '/dist/Ang10AppMultipleModules'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/Ang10appmultiplemodules/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Ang10AppMultipleModules/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
