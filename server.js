24c40921acc733ed864e8206782e3a994727395c
rajesh <rajesh.t7982@gmail.com>
2020-07-25 14:39:36 +0530

    updates to deploy to heroku by adding json-server1

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ server.use(middlewares);
 server.use(router);
 
 // Serve only the static files form the dist directory
-//app.use(express.static(__dirname + '/dist/Ang10AppMultipleModules'));
+server.use(express.static(__dirname + '/dist/Ang10AppMultipleModules'));
 
-//app.get('/*', function(req,res) {
+server.get('/*', function(req,res) {
     
-//res.sendFile(path.join(__dirname+'/dist/Ang10AppMultipleModules/index.html'));
-//});
+res.sendFile(path.join(__dirname+'/dist/Ang10AppMultipleModules/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 server.listen(process.env.PORT || 8080);
