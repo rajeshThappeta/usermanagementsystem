ad2949f7c896a7d5a0855f5ac69f64e3c1e5b4fe
rajesh <rajesh.t7982@gmail.com>
2020-07-28 21:33:15 +0530

    updates to deploy to heroku23

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
-//Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-//const middlewares = jsonServer.defaults({static:`__dirname + '/dist/Ang10AppMultipleModules/index.html`});
-//const port = process.env.PORT || 3000;
-//server.use(middlewares);
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+// Run the app by serving the static files
+// in the dist directory
+server.use(middlewares);
 server.use(router);
-
-// Serve only the static files form the dist directory
-//server.use(express.static(__dirname + '/dist/Ang10AppMultipleModules'));
-
-//server.get('/*', function(req,res) {
-    
-//res.sendFile(path.join(__dirname+'/dist/Ang10AppMultipleModules/index.html'));
-//});
-
-// Start the app by listening on the default Heroku port
+server.use(express.static(__dirname + '/dist/Ang10Appmultiplemodules'));
+// Start the app by listening on the default
+// Heroku port
 server.listen(process.env.PORT || 8080);
