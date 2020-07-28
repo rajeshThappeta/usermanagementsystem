da49c130e759c507937602fde996838e3ed38328
rajesh <rajesh.t7982@gmail.com>
2020-07-28 21:59:34 +0530

    updates to deploy to heroku34

--- server.js
+++ server.js
@@ -4,11 +4,26 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const path=require("path");
 // Run the app by serving the static files
 // in the dist directory
-server.use(middlewares);
-server.use(router);
-server.use(express.static(__dirname + '/dist/Ang10Appmultiplemodules'));
+//server.use(middlewares);
+//server.use(router);
+//server.use(express.static(__dirname + '/dist/Ang10Appmultiplemodules'));
+
+server.use( express.static(path.join(__dirname, '/dist/Ang10Appmultiplemodules')))
 // Start the app by listening on the default
+// Avoid CORS issue
+server.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+  server.get("/",function (req, res){
+    res.sendFile(__dirname + "/dist/Ang10Appmultiplemodules/index.html")
+    });
+ // server.use(jsonServer.rewriter(routes))
+  server.use(router)
 // Heroku port
-server.listen(process.env.PORT || 8080);
+server.listen( 4000);
